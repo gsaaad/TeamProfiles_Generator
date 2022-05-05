a7f59f39a9a37a21920a754f53195ac50ddb60ef
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-05 17:45:45 -0400

    added some basic code functionality, tested Employee Class

---
+++ __tests__/employee.js
@@ -0,0 +1,17 @@
+const Employee = require("../lib/Employee");
+
+// ! ASK TA
+// jest.mock("../lib/Employee");
+console.log(new Employee());
+test("Creates a new Employee Object", () => {
+  const employee = new Employee();
+  console.log(employee);
+
+  expect(employee.name).toBe("George");
+  expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
+  expect(employee.getName()).toEqual(expect.any(String));
+  expect(employee.getEmail()).toEqual(expect.any(String));
+  expect(employee.getId()).toEqual(expect.any(Number));
+  expect(employee.getRole()).toEqual("Employee");
+});
---
+++ mocks/Employee.js
@@ -0,0 +1,5 @@
+module.exports = function () {
+  this.name = "George";
+  this.id = 2;
+  this.email = "saad.george@live.com";
+};
