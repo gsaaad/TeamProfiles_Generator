4232c1553f41c4d33b77915ca907de676725d1ea
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-06 13:49:20 -0400

    asking user questions for creating the template

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 //node index.js runs the applications
+
+const teamProfileGenerator = require("./lib/teamProfileGenerator");
+const inquirer = require("inquirer");
+const newOne = new teamProfileGenerator();
+newOne.initializeQuestions().then((answers) => {
+  console.log(answers);
+  inquirer
+    .prompt({
+      type: "list",
+      name: "addRole",
+      message:
+        "Would you like to enter some information about an Enginner, a Intern, or finish?",
+      choices: ["Engineer", "Intern", "Finish Template"],
+    })
+    .then((addRole) => {
+      console.log(addRole);
+    });
+});
