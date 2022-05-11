58f991b921f25f26912cb8df5c2dd71d934d2d6b
gsaaad <67985748+gsaaad@users.noreply.github.com>
2022-05-11 07:50:02 -0400

    updating functionality and modularizing questions while pushing answers into objects

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 //node index.js runs the applications
 
 const teamProfileGenerator = require("./lib/teamProfileGenerator");
-const inquirer = require("inquirer");
+
+var EmployeesArray = [];
+
 const newOne = new teamProfileGenerator();
-newOne.initializeQuestions().then((answers) => {
-  console.log(answers);
-  inquirer
-    .prompt({
-      type: "list",
-      name: "addRole",
-      message:
-        "Would you like to enter some information about an Enginner, a Intern, or finish?",
-      choices: ["Engineer", "Intern", "Finish Template"],
-    })
-    .then((addRole) => {
-      console.log(addRole);
-    });
-});
+
+// newOne.initializeQuestions().then((answers) => {
+//   console.log(answers);
+//   EmployeesArray.push(answers);
+
+//   newOne.menuQuestions().then((addRole) => {
+//     let roleAnswer = addRole.addRole;
+
+//     if (roleAnswer === "Engineer") {
+//       newOne.engineerQuestions().then((engAnswers) => {
+//         console.log(engAnswers);
+//         newOne.menuQuestions();
+//       });
+//     }
+
+//     if (roleAnswer === "Intern") {
+//       newOne.internQuestions().then((internAnswers) => {
+//         console.log(internAnswers);
+//         newOne.menuQuestions();
+//       });
+//     } else {
+//       return;
+//     }
+//   });
+newOne.managerQuestions();
+
+// let initialAnswers = answers;
+// console.log(initialAnswers);
+// let engineerAnswers = engAnswers;
+// console.log(engineerAnswers);
+// let internAnswers = internAnswers;
+// console.log(internAnswers);
+// });
